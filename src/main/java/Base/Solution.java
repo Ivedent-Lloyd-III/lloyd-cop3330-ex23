af6b240e1eb8e06779364ed2e84c1367cfb96e30
Ivedent Lloyd III <ivedent.lloyd@gmail.com>
2021-06-06 20:13:50 -0400

    Initial commit

---
+++ src/main/java/Base/Solution.java
@@ -0,0 +1,20 @@
+package Base;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution 23
+ *  Copyright 2021 Ivedent Lloyd III
+ */
+
+import java.util.Scanner;
+
+public class Solution {
+
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args){
+
+        
+
+    }
+
+}
