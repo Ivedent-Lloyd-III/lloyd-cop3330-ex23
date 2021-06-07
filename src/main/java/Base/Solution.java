4374b6ae9cf9c2d61f204044c9725ce4a33cb1e8
Ivedent Lloyd III <ivedent.lloyd@gmail.com>
2021-06-06 21:46:35 -0400

    solution to ex23

--- src/main/java/Base/Solution.java
+++ src/main/java/Base/Solution.java
@@ -13,7 +13,85 @@ public class Solution {
 
     public static void main(String[] args){
 
-        
+        System.out.println("Is the car silent when you turn the key?. ");
+        String userInput = in.nextLine();
+
+        if(userInput.equals("Y") || userInput.equals("y")) {
+
+            System.out.println("Are the battery terminals corroded?. ");
+            userInput = in.nextLine();
+
+            if(userInput.equals("Y") || userInput.equals("y")) {
+
+                System.out.println("Clean the terminals and try starting again. ");
+
+            }
+
+            else{
+
+                System.out.println("Replace the cables and try again. ");
+
+            }
+
+        }
+
+        else{
+
+            System.out.println("Does the car make a ticking noise? ");
+            userInput = in.nextLine();
+
+            if(userInput.equals("Y") || userInput.equals("y")){
+
+                System.out.println("Replace the battery. ");
+
+            }
+
+            else{
+
+                System.out.println("Does the car crank up but fail to start? ");
+                userInput = in.nextLine();
+
+                if (userInput.equals("Y") || userInput.equals("y")){
+
+                    System.out.println("Check the spark plug connections. ");
+
+                }
+
+                else{
+
+                    System.out.println("Does the engine start and then die? ");
+                    userInput = in.nextLine();
+
+                    if(userInput.equals("Y") || userInput.equals("y")){
+
+                        System.out.println("Does the car have fuel injection? ");
+                        userInput = in.nextLine();
+
+                        if(userInput.equals("Y") || userInput.equals("y")){
+
+                            System.out.println("Get it in for service? ");
+
+                        }
+
+                        else{
+
+                            System.out.println("Check to ensure the choke is opening and closing? ");
+
+                        }
+
+                    }
+
+                    else{
+
+                        System.out.println("This should not be possible ");
+
+                    }
+
+                }
+
+            }
+
+        }
 
     }
 
